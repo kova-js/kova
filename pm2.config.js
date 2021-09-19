750528b9cfa16050c60e79352c374f2f4d074296
Zack <mwl@live.com>
2021-09-20 02:52:12 +0800

    chore: update pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -1,9 +1,7 @@
-const bundleInfo = require('./package.json')
-
 module.exports = {
   apps: [
     {
-      name: bundleInfo.name,
+      name: 'kova',
       script: 'dist/main.js',
       exec_mode: 'cluster',
       max_memory_restart: '800M',
