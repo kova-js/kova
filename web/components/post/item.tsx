41bcc4e81dc04aa3262dcb7a7a83eb89c31693d3
Zack <mwl@live.com>
2021-09-20 22:44:30 +0800

    chore: update posts

--- web/components/post/item.tsx
+++ web/components/post/item.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { FC } from 'react'
 import { Tag, Avatar } from 'antd'
 import { EyeOutlined, CommentOutlined } from '@ant-design/icons'
 import './item.less'
+import { NavLink } from 'react-router-dom'
 
 interface PostItemProps {
   data: any
 }
 
+function linkUrl(slug: string) {
+  return `/post/${slug}`
+}
+
+function userLinkUrl(name: string) {
+  return `/user/${name}`
+}
+
 const PostItem: FC<PostItemProps> = (props) => {
+  const { data = {} } = props
+  const { title, slug, cover, image, user = {} } = data
+
   return (
     <div className="post-item">
       <div
@@ -21,35 +33,26 @@ const PostItem: FC<PostItemProps> = (props) => {
         <div className="post-item-body">
           <div className="post-item-cate">
             <Tag>tt</Tag>
-            <a
-              href="https://cosy.demo.nicetheme.xyz/blog/108"
-              title="Windows 10X延期发布，微软这次做系统能学乖吗？"
-              className="post-item-title"
-            >
-              Windows 10X延期发布，微软这次做系统能学乖吗？
-            </a>
+            <NavLink to={linkUrl(slug)} title={title} className="post-item-title">
+              {title}
+            </NavLink>
           </div>
         </div>
         <div className="post-item-meta">
-          <a
-            href="https://cosy.demo.nicetheme.xyz/blog/author/suxing"
-            target="_blank"
-            className="post-item-avatar"
-          >
+          <NavLink to={userLinkUrl(user.name)} target="_blank" className="post-item-avatar">
             <Avatar
               size={24}
               alt=""
               src="https://cravatar.cn/avatar/db0a42e95e32035a8bb677ece1186533?s=24&amp;d=https%3A%2F%2Fcosy.demo.nicetheme.xyz%2Fwp-content%2Fuploads%2F2021%2F08%2F2021080111213873.png&amp;r=g"
             />
             <div className="post-item-avatar-name">suxing</div>
-          </a>
+          </NavLink>
           <div className="flex-shrink-0">
             <span className="d-inline-block d-xl-inline-block ms-2 ms-xl-3">
-              <EyeOutlined />
-              2,819
+              <EyeOutlined /> {data.browseCount || 0}
             </span>
             <span className="d-none d-xl-inline-block ms-2 ms-xl-3">
-              <CommentOutlined /> 1
+              <CommentOutlined /> {data.commentsCount || 0}
             </span>
           </div>
         </div>
