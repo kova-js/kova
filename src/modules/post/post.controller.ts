dddef120d7c4bbdf6fbf00db8e5976f802cfaacb
Zack <mwl@live.com>
2021-09-21 01:06:40 +0800

    chore: update admin

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -24,12 +24,33 @@ export class PostController {
     }
   }
 
-
   @Get('/write')
   @SsrRender()
   async write() {
     return {
       // pageProps,
     }
+  }
+
+  @Get('/rss')
+  async rss() {
+    return `<?xml version="1.0" encoding="UTF-8" ?>
+    <rss version="2.0">
+    <channel>
+      <title>菜鸟教程首页</title>
+      <link>https://fishbone.live/</link>
+      <description>Fishbone 的技术博客</description>
+      <item>
+        <title>基于Egg.js的博客增加RSS功能</title>
+        <link>https://fishbone.live/article/9</link>
+        <description>什么是 RSS ?</description>
+      </item>
+      <item>
+        <title>博客增加评论功能</title>
+        <link>https://fishbone.live/article/10</link>
+        <description>如何基于React，NodeJs，MySQL，开发一个类似于掘金的评论模块</description>
+      </item>
+    </channel>
+  </rss>`
   }
 }
