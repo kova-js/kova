2e9e993377e00c082b5d0ecd9cd81562c00a62dd
Zack <mwl@live.com>
2021-09-25 00:14:43 +0800

    chore: update nossr

--- web/components/UI/NoSsr/index.tsx
+++ web/components/UI/NoSsr/index.tsx
@@ -1,13 +1,36 @@
-import React, { ReactNodeArray } from 'react'
-import { FC, ReactNode } from 'react'
+import { useEnhancedEffect } from '@/hooks'
+import React, { FC, Fragment, ReactNode, useEffect, useState } from 'react'
 
-interface NoSsrProps {
+export type NoSsrProps = {
+  children: ReactNode
   defer?: boolean
-  children?: ReactNode | ReactNodeArray
+  fallback?: ReactNode
+  onMounted?: (mounted: boolean) => void
 }
 
 const NoSsr: FC<NoSsrProps> = (props) => {
-  return <>{props.children}</>
+  const { children, defer = false, fallback = null, onMounted } = props
+  const [mountedState, setMountedState] = useState(false)
+
+  useEnhancedEffect(() => {
+    if (!defer) {
+      setMountedState(true)
+    }
+  }, [defer])
+
+  useEffect(() => {
+    if (defer) {
+      setMountedState(true)
+    }
+  }, [defer])
+
+  useEffect(() => {
+    if (onMounted) {
+      onMounted(mountedState)
+    }
+  }, [mountedState, onMounted])
+
+  return <Fragment>{mountedState ? children : fallback}</Fragment>
 }
 
 export default NoSsr
