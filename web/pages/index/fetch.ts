a2c02c1126daa8be7f6c89e20313fc740103a3fb
Zack <mwl@live.com>
2021-09-25 00:42:38 +0800

    chore: update post item

--- web/pages/index/fetch.ts
+++ web/pages/index/fetch.ts
@@ -1,13 +1,15 @@
 import { WrapFetch } from '@/core/fetch'
 import { HomeProps } from './config'
 import { matchPath } from 'react-router'
+import axios from 'axios'
 
 export default WrapFetch<HomeProps>(async (ctx: any) => {
   const match = matchPath('/', { path: '/' })
   console.log(match)
+  const data = __isBrowser__ ? (await axios.get('/api/posts')).data : ctx.pageProps
   return {
     home: {
-      posts: ctx.pageProps,
+      posts: data,
     },
     props: {},
   }
