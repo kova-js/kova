9b762d046d846f220e3fe9cc0b39f2454219b4e2
Zack <mwl@live.com>
2021-09-25 00:47:47 +0800

    chore: update post item

--- web/core/emits/index.ts
+++ web/core/emits/index.ts
@@ -2,10 +2,10 @@ import mitt, { Emitter as MittEmitter } from 'mitt'
 export const emitter: MittEmitter<Record<string, any>> = mitt()
 
 if (__isBrowser__) {
-  emitter.on('layout', () => {
-    console.log('layout')
-  })
   emitter.on('page', () => {
     console.log('page')
+  })
+  emitter.on('layout', () => {
+    console.log('layout')
   })
 }
