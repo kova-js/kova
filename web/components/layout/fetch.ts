0c81d1a0f9ac8c523006e8ee9bc0e7bb03e718bb
Zack <mwl@live.com>
2021-09-25 11:01:59 +0800

    feat: update

--- web/components/layout/fetch.ts
+++ web/components/layout/fetch.ts
@@ -1,28 +1,24 @@
 import { emitter, useResolveRoute } from '@/hooks'
 import { promiseTimeout } from '@/utils'
-import { identity, pickBy} from 'lodash'
+import { identity, pickBy } from 'lodash'
 
 let hasRender = __isBrowser__ && !!window.__USE_SSR__
 
 const layoutFetch = async (ctx: any) => {
-  try {
-    const route = useResolveRoute(ctx)
-    let payload: Record<string, any> = {
-      route,
-    }
-    if (!hasRender) {
-      payload = Object.assign(payload, {
-        settings: {
-          name: 'Kova',
-        },
-      })
-      hasRender = true
-    }
-    await promiseTimeout(200)
-    return pickBy(payload, identity)
-  } catch (e) {
-    console.log(e)
+  const route = useResolveRoute(ctx)
+  let payload: Record<string, any> = {
+    route,
   }
+  if (!hasRender) {
+    payload = Object.assign(payload, {
+      settings: {
+        name: 'Kova',
+      },
+    })
+    hasRender = true
+  }
+  await promiseTimeout(200)
+  return pickBy(payload, identity)
 }
 
 export default function (ctx: any) {
