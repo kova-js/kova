2855f40bbcb5a9748f7157e01f3f98a8d6ed3983
Zack <mwl@live.com>
2021-09-30 21:46:26 +0800

    feat: update config

--- libs/cli/index.js
+++ libs/cli/index.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const path = require('path')
 const { getCwd } = require('ssr-server-utils')
 
-const config = () => {
+const loadConfig = () => {
   return yaml.load(fs.readFileSync(path.resolve(getCwd(), 'config.yml'), 'utf8'))
 }
 
-module.exports = { config }
+module.exports = { loadConfig }
