f02241cb25368eeb6e8ad8e82a0634436432fefd
Zack <mwl@live.com>
2021-10-16 15:42:42 +0800

    feat: update admin layout

--- web/layouts/admin/index.vue
+++ web/layouts/admin/index.vue
@@ -1,12 +1,101 @@
 <template>
-    <div class="admin">
-        <slot />
-    </div>
+  <a-layout style="min-height: 100vh">
+    <a-layout-sider v-model:collapsed="collapsed" collapsible>
+      <div class="logo" />
+      <a-menu theme="dark" v-model:selectedKeys="selectedKeys" mode="inline">
+        <a-menu-item key="1">
+          <pie-chart-outlined />
+          <span>Option 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <desktop-outlined />
+          <span>Option 2</span>
+        </a-menu-item>
+        <!-- <a-sub-menu key="sub1">
+          <template #title>
+            <span>
+              <user-outlined />
+              <span>User</span>
+            </span>
+          </template>
+          <a-menu-item key="3">Tom</a-menu-item>
+          <a-menu-item key="4">Bill</a-menu-item>
+          <a-menu-item key="5">Alex</a-menu-item>
+        </a-sub-menu> -->
+        <!-- <a-sub-menu key="sub2">
+          <template #title>
+            <span>
+              <team-outlined />
+              <span>Team</span>
+            </span>
+          </template>
+          <a-menu-item key="6">Team 1</a-menu-item>
+          <a-menu-item key="8">Team 2</a-menu-item>
+        </a-sub-menu> -->
+        <a-menu-item key="9">
+          <file-outlined />
+          <span>File</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0" />
+      <a-layout-content style="margin: 0 16px">
+        <!-- <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>User</a-breadcrumb-item>
+          <a-breadcrumb-item>Bill</a-breadcrumb-item>
+        </a-breadcrumb> -->
+        <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
+          Bill is a cat.
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import { Layout, Menu } from 'ant-design-vue'
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  MenuUnfoldOutlined,
+  TeamOutlined,
+  DesktopOutlined,
+  MenuFoldOutlined,
+  PieChartOutlined,
+  FileOutlined,
+} from '@ant-design/icons-vue'
 export default defineComponent({
-    name: "AdminLayout"
+  name: 'AdminLayout',
+  components: {
+    PieChartOutlined,
+    UserOutlined,
+    DesktopOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+    TeamOutlined,
+    FileOutlined,
+    ALayout: Layout,
+    ALayoutFooter: Layout.Footer,
+    ALayoutHeader: Layout.Header,
+    ALayoutContent: Layout.Content,
+    ALayoutSider: Layout.Sider,
+    AMenu: Menu,
+    AMenuItem: Menu.Item,
+    ASubMenu: Menu.SubMenu
+  },
+  setup(props) {
+    return {
+      selectedKeys: ref<string[]>(['1']),
+      collapsed: ref<boolean>(false),
+    }
+  },
 })
 </script>
